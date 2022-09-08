5e8c804e00cbcc30f52f485c3c3fc81036dece33
notGabo <ga.soto@duocuc.cl>
2022-09-08 00:44:00 -0400

    Fix css

--- app/pages/configuracion/configuracion.page.ts
+++ app/pages/configuracion/configuracion.page.ts
@@ -19,8 +19,6 @@ export class ConfiguracionPage implements OnInit {
       subHeader: 'Somos un equipo de 2 estudiantes que estamos desarrollando esta aplicación para la asignatura de Desarrollo de apliciones moviles de manera hibrida.',
       message: '<img src="./assets/logo.png" class="logo" alt="">',
       cssClass: 'aboutUs'
-
-
     });
     await alert.present();
   }
