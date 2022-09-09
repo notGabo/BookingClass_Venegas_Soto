0ca174a1ddc592e2d8f15824e719e5b6affcc530
notGabo <ga.soto@duocuc.cl>
2022-09-08 20:46:35 -0400

    Fix menu bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 interface Componente {
   icon: string;
   name: string;
@@ -12,7 +14,7 @@ interface Componente {
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private menuController: MenuController) { }
+  constructor(private menuController: MenuController, private router: Router) { }
   componentes: Componente[] = [
     {
       icon: 'home-outline',
@@ -31,4 +33,16 @@ export class AppComponent {
     }
   ];
 
+  public hasMenu = true;
+
+  ngOnInit() {
+    this.router.events.subscribe((event: any) => {
+      if (event.url in ['/', '/codigo-qr', '/escanear-qr', '/configuracion', '/generador-qr', '/home-alumno', '/home-profesor', '/login-alumno', '/login-profesor', '/menu-alumno']) {
+        this.hasMenu = false;
+      }
+      else {
+        this.hasMenu = true;
+      }
+    });
+  };
 }
